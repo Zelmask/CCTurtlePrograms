14717ea64cb4a3f22f20d1a9b79035fc99bbbf88
Zelmask <killer72@gmail.com>
2020-11-29 06:59:07 -0500

    Commit code files + early acces du Patcher

---
+++ patcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace patcher
+{
+    class Program
+    {
+        private static string path;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the patcher");
+            string rootDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var potato = Directory.EnumerateFiles(rootDir, "*", SearchOption.AllDirectories).ToList();
+            var codeFiles = Directory.EnumerateDirectories(rootDir).SelectMany(x=> Directory.EnumerateFiles(x, "*", SearchOption.AllDirectories)).ToList();
+            Console.WriteLine(codeFiles);
+            path = Console.ReadLine();
+
+        }
+    }
+}
